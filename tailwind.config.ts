9c219d43790579aef98ad43ec1b47553df6f1235
Bhagya <bhagyabraj96@gmail.com>
2026-03-23 17:37:57 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./animations/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    screens: {
+      xm: { max: "400px" },
+      xs:{ max: "500px" },
+      sm: { min: "501px", max: "768px" },
+      md: { min: "769px", max: "1024px" },
+      lg: { min: "1025px", max: "1490px" },
+      xl: { min: "1491px" },
+      mdd: { min: "769px" },
+    }
+  },
+  plugins: [],
+};
+export default config;
